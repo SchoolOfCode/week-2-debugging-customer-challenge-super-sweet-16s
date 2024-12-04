db6d42d48157993ffe716c49c48003cd9325fcaf
“olivcat” <184215778+olivcat@users.noreply.github.com>
2024-12-04 16:04:48 +0000

    Debugged code

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 let score = 0;
 let playAgain = true;
 
-while (playAgain === false) 
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
+while (playAgain === false) {
+  const randomNumber = Math.floor(Math.random() * 10) + 1; // changed from 100 to 10 so that the range is 1-10 instead of 1-100
   const userGuess = prompt("Guess a number between 1 and 10:");
 
-  if (userGuess = randomnumber) { // typo - should be randomNumber
+  if (userGuess === randomNumber) {
+    // typo - should be randomNumber and added ===
     alert("Congratulations! You guessed the correct number.");
     score++;
   } else {
-    alert(`Sorry, the correct number was ${randomnumber}. You lose.`); //typo - should be randomNumber 
-    score++;
+    alert(`Sorry, the correct number was ${randomnumber}. You lose.`); //typo - should be randomNumber
+    // take out score++;
   }
 
   playAgain = confirm("Do you want to play again?");
 
-  if (!playAgain); { // typo - shouldn't be an exclamation mark at the beginning?
+  if (!playAgain);
+  {
     alert(`Game over. Your final score is ${randomNumber}.`);
   }
-} // rogue bracker
+}
